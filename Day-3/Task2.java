373ae09b45fa4fbede469828c774abf64d7c9dc8
Praveenkumar-7777 <praveenkumar3166089@gmail.com>
2026-04-29 11:35:46 +0530

    Check Eligibility

---
+++ Day-3/Task2.java
@@ -0,0 +1,11 @@
+class Task2{
+    public static void main(String[] args) {
+        int age = 77;
+        if(age>=18){
+            System.out.println("Eligible to Vote");
+        }
+        else{
+            System.out.println("Not eligible to Vote");
+        }
+    }
+}
